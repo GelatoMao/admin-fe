0cf5d96051d4ff6a18a1099d06f5cb05d0471426
GelatoMao <gelatomao@outlook.com>
2020-09-15 21:32:30 +0800

    base page

--- src/component/top-nav/index.jsx
+++ src/component/top-nav/index.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import MUtil from "util/mm.jsx";
+import User from "service/user-service.jsx";
+
+const _mm = new MUtil();
+const _user = new User();
 
 export default class TopNav extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: _mm.getStorage("userInfo").username || "",
+    };
   }
 
   //退出登录
-  onLogout(){}
+  onLogout() {
+    _user.logout().then(
+      (res) => {
+        _mm.removeStorage("userInfo");
+        window.location.href = "/login"; 
+      },
+      (errMsg) => {
+        _mm.errorTips(errMsg);
+      }
+    );
+  }
   render() {
     return (
       <div className="navbar navbar-default top-navbar">
@@ -22,7 +39,11 @@ export default class TopNav extends Component {
           <li className="dropdown">
             <a className="dropdown-toggle" href="javascript:;">
               <i className="fa fa-user fa-fw"></i>
-              <span>欢迎，adminxxxx</span>
+              {this.state.username ? (
+                <span>欢迎，{this.state.username}</span>
+              ) : (
+                <span>欢迎您</span>
+              )}
               <i className="fa fa-caret-down"></i>
             </a>
             <ul className="dropdown-menu dropdown-user">
